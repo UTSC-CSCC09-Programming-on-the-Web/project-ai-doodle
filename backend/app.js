5705a8d88ac80a354c410ae7e33a9815cdfcb631
Wengxin04 <wengxin0421@outlook.com>
2025-06-24 00:40:55 -0400

    initialize postgresSQL database

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,30 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
 dotenv.config();
+
+import { testConnection, sequelize } from './models/datasource.js'
+import { User } from './models/user.js'
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Backend is running' });
-});
+// test
+app.get('/api/hello', async (req, res) => {
+  const user = await User.create({ username: 'test1', nickname: 'Test User' })
+  res.json({ message: `User ${user.username} created` })
+})
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const init = async () => {
+  await testConnection()
+  await sequelize.sync({ alter: true })
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+  })
+}
+
+init()
---
+++ backend/models/user.js
@@ -0,0 +1,14 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from './datasource.js'
+
+// more fields need to be added later
+export const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  nickname: {
+    type: DataTypes.STRING,
+  },
+})
